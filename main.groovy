690083a36f5be7ec1b8dd9f62214fe91d23a32f7
Rafael Bacelar <rafael.c.bacelar@gmail.com>
2019-10-18 15:37:14 -0300

    Fix job name

--- main.groovy
+++ main.groovy
@@ -1,7 +1,7 @@
 def  files = ['test1.jenkinsfile', 'test2.jenkinsfile']
 
 files.each { file ->
-    pipelineJob(file.getName()) {
+    pipelineJob(file.replace('.jenkinsfile', '')) {
         definition {
             cps {
                 script(readFileFromWorkspace(file).stripIndent())
